376932a91f99b1ea900e91249e5a0e8bac5e89e1
mhadih <m.hadi.hojjat@gmail.com>
2019-03-27 16:01:16 +0430

    generate ANTLR

---
+++ gen/ToorlaVisitor.java
@@ -0,0 +1,48 @@
+// Generated from /home/hadi/Desktop/compiler/compiler_phase1/PLC_Spring98_Toorla/grammar/Toorla.g4 by ANTLR 4.7.2
+
+    import toorla.ast.expression.binaryExpression.*;
+    import toorla.ast.expression.value.*;
+    import toorla.ast.expression.*;
+    import toorla.ast.statement.*;
+
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link ToorlaParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface ToorlaVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link ToorlaParser#program}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitProgram(ToorlaParser.ProgramContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ToorlaParser#additionExp}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAdditionExp(ToorlaParser.AdditionExpContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ToorlaParser#temp_add}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTemp_add(ToorlaParser.Temp_addContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ToorlaParser#multiplyExp}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitMultiplyExp(ToorlaParser.MultiplyExpContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ToorlaParser#atomExp}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAtomExp(ToorlaParser.AtomExpContext ctx);
+}
